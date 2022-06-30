8689bfd6603cab8823cf937c30eccede645c46a8
Androsh <apanasiuk00@gmail.com>
2022-06-29 21:42:10 -0300

    Update

--- src/App.js
+++ src/App.js
@@ -10,7 +10,14 @@ function App() {
     <Router>
         <div className="flex flex-col justify-between h-screen">
           <Navbar />
-          <main className="container mx-auto px-3 pb-12">Content</main>
+          <main className="container mx-auto px-3 pb-12">
+            <Routes>
+              <Route path='/'element={<Home />} />
+              <Route path='/about'element={<About />} />
+              <Route path='/notfound'element={<NotFound />} />
+              <Route path='/*'element={<NotFound />} />
+            </Routes>
+          </main>
           <Footer />
         </div>
     </Router>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,9 @@
 
 function Home() {
   return (
-    <div>Home</div>
+    <div>
+      <h1 className="text-6xl">Welcome</h1>
+    </div>
   )
 }
 
